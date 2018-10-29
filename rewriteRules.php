25ac7509b7fac6d310d4f9b5b2d846ddb38437fd
Bart Jansen <bart@levato.nl>
2018-10-29 11:10:30 +0000

    Removed unused files

--- rewriteRules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: Laveto, LaravelMultivers, laveto, laravelmultivers
-return [
-    'src/MyPackage.php' => 'src/LaravelMultivers.php',
-    'config/mypackage.php' => 'config/laravelmultivers.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/LaravelMultivers.php',
-    'src/MyPackageServiceProvider.php' => 'src/MultiversServiceProvider.php',
-];
